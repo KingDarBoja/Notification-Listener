2a62639ea26a893a0444327124fdad55cd71d766
KingDarBoja <mbojato@uninorte.edu.co>
2019-01-26 13:24:59 -0500

    Initial Commit and Setup of Notification Library

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:notifications/notifications.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    Notifications notifications = new Notifications();
+    StreamSubscription<NotificationEvent> events;
+    events = notifications.noiseStream.listen(onData);
+  }
+
+  void onData(NotificationEvent event) => print(event.toString());
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+      ),
+    );
+  }
+}
